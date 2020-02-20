a743754c1ef0685c0cee2db1e336e921e1da006f
Oguzhan <oguzhanturgut@outlook.com>
2020-02-21 00:31:47 +0100

    add userplace page and components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-d
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UserPlaces from './places/pages/UserPlaces';
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Switch>
           <Route path='/' exact>
             <Users />
+          </Route>
+          <Route path='/:userId/places'>
+            <UserPlaces />
           </Route>
           <Route path='/places/new' exact>
             <NewPlace />
---
+++ frontend/src/places/pages/UserPlaces.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+  {
+    id: 'p1',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world',
+    imageUrl:
+      'https://www.newyork.nl/wp-content/uploads/2013/11/Empire-State-Building-Tickets-300x226.jpg',
+    address: '20 W 34th St, New York NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u1',
+  },
+  {
+    id: 'p2',
+    title: 'Empire State Building',
+    description: 'One of the most famous sky scrapers in the world',
+    imageUrl:
+      'https://www.newyork.nl/wp-content/uploads/2013/11/Empire-State-Building-Tickets-300x226.jpg',
+    address: '20 W 34th St, New York NY 10001',
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878584,
+    },
+    creator: 'u2',
+  },
+];
+
+const UserPlaces = () => {
+  const { userId } = useParams();
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+  return <PlaceList items={loadedPlaces} />;
+};
+
+export default UserPlaces;
