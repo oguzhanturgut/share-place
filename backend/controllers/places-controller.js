850eeafb4a68d811b9767fc128094ae4d614abff
Oguzhan <oguzhanturgut@outlook.com>
2020-02-27 15:58:48 +0100

    add geolocation api

--- backend/controllers/places-controller.js
+++ backend/controllers/places-controller.js
@@ -2,6 +2,7 @@ const { v4: uuid } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
   {
@@ -36,12 +37,20 @@ const getPlacesByUserId = (req, res, next) => {
   res.json({ places });
 };
 
-const createPlace = (req, res, next) => {
+const createPlace = async (req, res, next) => {
   const errors = validationResult(req);
   console.log(errors.mapped());
-  if (!errors.isEmpty()) throw new HttpError('Invalid value', 422);
+  if (!errors.isEmpty()) return next(new HttpError('Invalid value', 422));
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates;
+  try {
+    coordinates = await getCoordsForAddress(address);
+  } catch (error) {
+    return next(error);
+  }
+
   const newPlace = {
     id: uuid(),
     title,
