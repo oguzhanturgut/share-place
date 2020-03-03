de7fb5cdc9ba617f718cf92c4344c03dba1b8f7d
Oguzhan <oguzhanturgut@outlook.com>
2020-03-03 15:59:35 +0100

    add functionality to get all users

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,16 +1,40 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+
 import UsersList from '../components/UsersList';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Test User',
-      image: 'https://picsum.photos/200',
-      places: 3,
-    },
-  ];
-  return <UsersList items={USERS} />;
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [loadedUsers, setLoadedUsers] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        setIsLoading(true);
+        const response = await fetch('http://localhost:5000/api/users');
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message);
+        setLoadedUsers(data.users);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+  }, []);
+  return (
+    <Fragment>
+      <ErrorModal error={error} onClear={() => setError(null)} />
+      {isLoading && (
+        <div className='center'>
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedUsers && <UsersList items={loadedUsers} />};
+    </Fragment>
+  );
 };
 
 export default Users;
