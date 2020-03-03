b313c937bbeb10ca4d4d59a0714e213dbff855ff
Oguzhan <oguzhanturgut@outlook.com>
2020-03-03 15:43:46 +0100

    add functionality to login

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -38,10 +38,31 @@ const Auth = () => {
 
   const authSubmitHandler = async event => {
     event.preventDefault();
+    setIsLoading(true);
     if (isLoginMode) {
+      try {
+        const response = await fetch('http://localhost:5000/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+          }),
+        });
+
+        const data = await response.json();
+        if (!response.ok) throw new Error(data.message);
+        setIsLoading(false);
+        auth.login();
+      } catch (error) {
+        console.log(error);
+        setIsLoading(false);
+        setError(error.message || 'Something went wrong');
+      }
     } else {
       try {
-        setIsLoading(true);
         const response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -56,7 +77,6 @@ const Auth = () => {
 
         const data = await response.json();
         if (!response.ok) throw new Error(data.message);
-        console.log(data);
         setIsLoading(false);
         auth.login();
       } catch (error) {
