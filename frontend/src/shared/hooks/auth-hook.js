c0668eb8561c97a94f811615d3407a77ad6c9877
Oguzhan <oguzhanturgut@outlook.com>
2020-03-20 00:33:26 +0100

    minor changes

---
+++ frontend/src/shared/hooks/auth-hook.js
@@ -0,0 +1,54 @@
+import {useState, useEffect, useCallback} from 'react';
+
+let logoutTimer;
+
+export const useAuth = () => {
+  const [token, setToken] = useState(null);
+  const [tokenExpiration, setTokenExpiration] = useState(null);
+  const [userId, setUserId] = useState(false);
+
+  const login = useCallback((uuid, token, expirationDate) => {
+    setToken(token);
+    setUserId(uuid);
+    const tokenExpirationDate =
+      expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpiration(tokenExpirationDate);
+    localStorage.setItem(
+      "userData",
+      JSON.stringify({
+        userId: uuid,
+        token,
+        expiration: tokenExpirationDate.toISOString()
+      })
+    );
+  }, []);
+
+  const logout = useCallback(() => {
+    setToken(null);
+    setTokenExpiration(null);
+    setUserId(null);
+    localStorage.removeItem("userData");
+  }, []);
+
+  useEffect(() => {
+    if (token && tokenExpiration) {
+      logoutTimer = setTimeout(logout, tokenExpiration.getTime() - new Date().getTime());
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpiration]);
+
+
+  useEffect(() => {
+    const storedData = JSON.parse(localStorage.getItem("userData"));
+    if (
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
+    ) {
+      login(storedData.userId, storedData.token);
+    }
+  }, [login]);
+
+  return [token, login, logout, userId];
+};
